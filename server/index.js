161cebc08da757ba56be1005805fa33fe5d6107a
rafdhiyaulhaq <m.rafidhiyaulhaq@gmail.com>
2025-01-07 19:46:47 +0700

    docker config

--- server/index.js
+++ server/index.js
@@ -7,16 +7,23 @@ const seedProducts = require('./seed/products');
 
 const app = express();
 const productRoutes = require('./routes/productRoutes');
-const authRoutes = require('./routes/authRoutes'); 
+const authRoutes = require('./routes/authRoutes');
 const quizRoutes = require('./routes/quizRoutes');
 const Product = require('./models/Product');
 
 app.use(cors({
-  origin: ['https://skinmatch-five.vercel.app', 'http://localhost:5173'],
+  origin: [
+    'https://skinmatch-five.vercel.app', 
+    'http://localhost:3000',
+    'http://localhost:5173',
+    'http://localhost:80'
+  ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
+  credentials: true,
+  exposedHeaders: ['Access-Control-Allow-Origin']
 }));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
